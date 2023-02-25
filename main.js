2f6862dabffd8bf4e84072bdb2a92b9700ed6513
Kostiantyn Sokolov <del12dmc@gmail.com>
2023-02-25 18:20:06 +0200

    book automation training

---
+++ main.js
@@ -0,0 +1,12 @@
+const godFather = "Don";
+const youngestSonOfGodFather = "Michael Corleone";
+const youngestSonOfGodFatherNameOnly = "Michael";
+const familyNameOfGodFather = "Corleone";
+
+document.body.innerHTML = `
+    <p>${youngestSonOfGodFather} came to the garden alone, as his father had requested. It was early in the morning, the air still and cool, and the garden was empty except for the old ${godFather} sitting in his wooden chair, his head bowed in prayer. ${youngestSonOfGodFatherNameOnly} took a seat opposite him, his eyes taking in the lush greenery and the sweet scent of the flowers.</p>
+    <p>After a few moments, the ${godFather} looked up and smiled at his son. "I always knew that one day you would be standing here in my place," he said quietly. "And now it is time for you to take over, to be the new Godfather. You have my blessing, and my trust."</p>
+    <p>${youngestSonOfGodFatherNameOnly} felt a surge of emotion at his father's words. He had always tried to distance himself from the family business, to forge his own path, but now he realized that he could not escape his destiny. He nodded solemnly, silently accepting his father's mantle.</p>
+    <p>As he sat alone in his office, ${youngestSonOfGodFatherNameOnly} couldn't help but reflect on the choices he had made, the decisions that had led him to this point. He had always been the black sheep of the family, the one who wanted to be a respectable businessman instead of a criminal mastermind. But in the end, he had been forced to take over the family business, to protect his father and his brothers.</p>
+    <p>Now, he was the new ${godFather}, the head of the <span class="accent">${familyNameOfGodFather} family</span>, and he knew that his enemies would be coming for him. But he was ready. He had learned from his father's mistakes, and he would not make the same ones. He would be ruthless, cunning, and strategic, but he would also be fair and just.</p>
+`
